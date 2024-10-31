261b53195b205474ffb16879d10797ea0b7ba05c
Catarina Crespo <ci.crespo@campus.fct.unl.pt>
2024-10-31 09:49:47 +0000

    Project Setup

---
+++ android/app/src/main/java/ionic/goventure/MainActivity.java
@@ -0,0 +1,5 @@
+package ionic.goventure;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'ionic.goventure',
+  appName: 'goventure',
+  webDir: 'dist'
+};
+
+export default config;
