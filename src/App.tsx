4b7817ff1c53b8a4b2bedbbce4864e4e62a8924f
Ricardo Bessa <r.bessa@campus.fct.unl.pt>
2024-11-15 02:20:53 +0000

    Profile and Prices

--- src/App.tsx
+++ src/App.tsx
@@ -14,16 +14,14 @@ import { ellipse, square, triangle } from 'ionicons/icons';
 import Tab1 from './pages/Tab1';
 import Tab2 from './pages/Tab2';
 import Tab3 from './pages/Tab3';
+import Profile from './pages/Profile'; 
+import Prices from './pages/Prices'; 
 
-/* Core CSS required for Ionic components to work properly */
+
 import '@ionic/react/css/core.css';
-
-/* Basic CSS for apps built with Ionic */
 import '@ionic/react/css/normalize.css';
 import '@ionic/react/css/structure.css';
 import '@ionic/react/css/typography.css';
-
-/* Optional CSS utils that can be commented out */
 import '@ionic/react/css/padding.css';
 import '@ionic/react/css/float-elements.css';
 import '@ionic/react/css/text-alignment.css';
@@ -31,18 +29,6 @@ import '@ionic/react/css/text-transformation.css';
 import '@ionic/react/css/flex-utils.css';
 import '@ionic/react/css/display.css';
 
-/**
- * Ionic Dark Mode
- * -----------------------------------------------------
- * For more info, please see:
- * https://ionicframework.com/docs/theming/dark-mode
- */
-
-/* import '@ionic/react/css/palettes/dark.always.css'; */
-/* import '@ionic/react/css/palettes/dark.class.css'; */
-import '@ionic/react/css/palettes/dark.system.css';
-
-/* Theme variables */
 import './styles/variables.css';
 
 setupIonicReact();
@@ -58,11 +44,17 @@ const App: React.FC = () => (
           <Route exact path="/tab2">
             <Tab2 />
           </Route>
-          <Route path="/tab3">
+          <Route exact path="/tab3">
             <Tab3 />
           </Route>
+          <Route exact path="/profile">
+            <Profile /> {/* Route for Profile page */}
+          </Route>
+          <Route exact path="/prices">
+            <Prices /> {/* Route for Prices page */}
+          </Route>
           <Route exact path="/">
-            <Redirect to="/tab1" />
+            <Redirect to="/prices" /> {/* Redirect to Prices page */}
           </Route>
         </IonRouterOutlet>
         <IonTabBar slot="bottom">
---
+++ src/pages/Prices.tsx
@@ -0,0 +1,164 @@
+import { useState } from 'react';
+import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar, IonList, IonItem, IonLabel, IonButton, IonIcon, IonGrid, IonRow, IonCol, IonCard, IonCardHeader, IonCardTitle, IonCardContent } from '@ionic/react';
+import { restaurant, bus, map, cart } from 'ionicons/icons';
+import '../styles/Prices.css';
+
+const Prices: React.FC = () => {
+  const [selectedCategory, setSelectedCategory] = useState<string | null>(null);
+
+  const handleCategoryChange = (value: string) => {
+    setSelectedCategory(value);
+  };
+
+  return (
+    <IonPage>
+      <IonHeader>
+        <IonToolbar>
+          <IonTitle>Prices</IonTitle>
+        </IonToolbar>
+      </IonHeader>
+      <IonContent fullscreen>
+        <IonHeader collapse="condense">
+          <IonToolbar>
+            <IonTitle size="large">Prices</IonTitle>
+          </IonToolbar>
+        </IonHeader>
+        
+        {!selectedCategory ? (
+          <IonGrid>
+            <IonRow>
+              <IonCol size="6">
+                <IonCard button onClick={() => handleCategoryChange('Food')}>
+                  <img src="src/styles/restaurant.jpg" alt="Food" className="hover-image" />
+                  <IonCardHeader>
+                    <IonCardTitle>Food</IonCardTitle>
+                  </IonCardHeader>
+                </IonCard>
+              </IonCol>
+              <IonCol size="6">
+                <IonCard button onClick={() => handleCategoryChange('Transport')}>
+                  <img src="src/styles/train.jpeg" alt="Transport" className="hover-image" />
+                  <IonCardHeader>
+                    <IonCardTitle>Transport</IonCardTitle>
+                  </IonCardHeader>
+                </IonCard>
+              </IonCol>
+            </IonRow>
+            <IonRow>
+              <IonCol size="6">
+                <IonCard button onClick={() => handleCategoryChange('Attractions')}>
+                  <img src="src/styles/museum.jpg" alt="Attractions" className="hover-image" />
+                  <IonCardHeader>
+                    <IonCardTitle>Attractions</IonCardTitle>
+                  </IonCardHeader>
+                </IonCard>
+              </IonCol>
+              <IonCol size="6">
+                <IonCard button onClick={() => handleCategoryChange('Supermarket')}>
+                  <img src="src/styles/supermarket.jpg" alt="Supermarket" className="hover-image" />
+                  <IonCardHeader>
+                    <IonCardTitle>Supermarket</IonCardTitle>
+                  </IonCardHeader>
+                </IonCard>
+              </IonCol>
+            </IonRow>
+          </IonGrid>
+        ) : (
+          <>
+            <IonList className="prices-list">
+              <IonItem>
+                <IonLabel className="category-title">{selectedCategory} Prices</IonLabel>
+              </IonItem>
+              {selectedCategory === 'Food' && (
+                <ul className="price-list">
+                  <li>
+                    <IonItem className="price-item">
+                      <IonLabel>Restaurant Meal: 15€</IonLabel>
+                    </IonItem>
+                  </li>
+                  <li>
+                    <IonItem className="price-item">
+                      <IonLabel>Fast Food: 8€</IonLabel>
+                    </IonItem>
+                  </li>
+                </ul>
+              )}
+              {selectedCategory === 'Transport' && (
+                <ul className="price-list">
+                  <li>
+                    <IonItem className="price-item">
+                      <IonLabel>Bus Ticket: 2€</IonLabel>
+                    </IonItem>
+                  </li>
+                  <li>
+                    <IonItem className="price-item">
+                      <IonLabel>Taxi (per km): 1.5€</IonLabel>
+                    </IonItem>
+                  </li>
+                </ul>
+              )}
+              {selectedCategory === 'Attractions' && (
+                <ul className="price-list">
+                  <li>
+                    <IonItem className="price-item">
+                      <IonLabel className="attraction-title">Colosseum</IonLabel>
+                    </IonItem>
+                    <ul>
+                      <li>
+                        <IonItem className="price-without-tour">
+                          <IonLabel>Without Tour: 12€</IonLabel>
+                        </IonItem>
+                      </li>
+                      <li>
+                        <IonItem className="price-with-tour">
+                          <IonLabel>With Tour: 20€</IonLabel>
+                        </IonItem>
+                      </li>
+                    </ul>
+                  </li>
+                  <li>
+                    <IonItem className="price-item">
+                      <IonLabel className="attraction-title">Museum</IonLabel>
+                    </IonItem>
+                    <ul>
+                      <li>
+                        <IonItem className="price-without-tour">
+                          <IonLabel>Without Tour: 10€</IonLabel>
+                        </IonItem>
+                      </li>
+                      <li>
+                        <IonItem className="price-with-tour">
+                          <IonLabel>With Tour: 18€</IonLabel>
+                        </IonItem>
+                      </li>
+                    </ul>
+                  </li>
+                </ul>
+              )}
+              {selectedCategory === 'Supermarket' && (
+                <ul className="price-list">
+                  <li>
+                    <IonItem className="price-item">
+                      <IonLabel>Milk (1L): 1€</IonLabel>
+                    </IonItem>
+                  </li>
+                  <li>
+                    <IonItem className="price-item">
+                      <IonLabel>Bread (500g): 1.5€</IonLabel>
+                    </IonItem>
+                  </li>
+                </ul>
+              )}
+            </IonList>
+            <IonButton expand="block" color="primary" onClick={() => setSelectedCategory(null)}>
+              Back to Selection
+            </IonButton>
+          </>
+        )}
+        
+      </IonContent>
+    </IonPage>
+  );
+};
+
+export default Prices;
