34fe4c6cd83c3b70f7b7aa55e263478fc6c572de
Catarina Crespo <ci.crespo@campus.fct.unl.pt>
2024-11-19 02:33:44 +0000

    Building the plan with JS

--- src/pages/CheckPlanPage.tsx
+++ src/pages/CheckPlanPage.tsx
@@ -13,12 +13,8 @@ import {
   IonTitle,
   IonToolbar,
 } from "@ionic/react";
-import ExploreContainer from "../components/ExploreContainer";
 import "../styles/CheckPlan.css";
 import {
-  add,
-  caretBack,
-  chevronBack,
   close,
   locationSharp,
   settings,
@@ -37,16 +33,53 @@ import TimelineOppositeContent, {
 import { Icon } from "@iconify/react";
 
 import image from "../img/sunny.jpg";
-
 import RegeneratePlan from "../components/RegeneratePlan";
-import Filters from "../components/Filters";
+
+import eventsData from '../data/events.json';
+
+// Helper function to convert duration string like "2h", "30min" into minutes
+const convertDurationToMinutes = (duration: string) => {
+  const timeParts = duration.split(" ");
+  let minutes = 0;
+
+  timeParts.forEach((part) => {
+    if (part.includes("h")) {
+      minutes += parseInt(part) * 60; // Convert hours to minutes
+    } else if (part.includes("min")) {
+      minutes += parseInt(part); // Add minutes directly
+    }
+  });
+
+  return minutes;
+};
+
+// Function to format time in "HH:mm"
+const formatTime = (minutes: number) => {
+  const hours = Math.floor(minutes / 60);
+  const mins = minutes % 60;
+  return `${hours}:${mins < 10 ? "0" + mins : mins}`;
+};
 
 const CheckPlanPage: React.FC = () => {
+  const [selectedDate, setSelectedDate] = React.useState<string>('');
+  const [filteredEvents, setFilteredEvents] = React.useState<any[]>([]);
+
+  // Handle date change
+  const handleDateChange = (e: any) => {
+    const date = e.detail.value;
+    setSelectedDate(date);
+
+    // Filter events based on the selected date
+    const eventsForSelectedDate = eventsData.filter(event => event.date === date);
+    setFilteredEvents(eventsForSelectedDate);
+  };
 
   const [open, setOpen] = React.useState(false);
-
   const handleOpen = () => setOpen(true);
   const handleClose = () => setOpen(false);
+
+  // Variable to keep track of the accumulated time (in minutes)
+  let accumulatedMinutes = 9 * 60; // Starting time is 9:00 (in minutes)
 
   return (
     <IonPage>
@@ -76,10 +109,15 @@ const CheckPlanPage: React.FC = () => {
               aria-label="Day"
               interface="popover"
               placeholder="Select a date"
+              onIonChange={handleDateChange}
             >
-              <IonSelectOption value="7">7 Sept</IonSelectOption>
-              <IonSelectOption value="8">8 Sept</IonSelectOption>
-              <IonSelectOption value="9">9 Sept</IonSelectOption>
+              <IonSelectOption value="7 Sept">7 Sept</IonSelectOption>
+              <IonSelectOption value="8 Sept">8 Sept</IonSelectOption>
+              <IonSelectOption value="9 Sept">9 Sept</IonSelectOption>
+              <IonSelectOption value="10 Sept">10 Sept</IonSelectOption>
+              <IonSelectOption value="11 Sept">11 Sept</IonSelectOption>
+              <IonSelectOption value="12 Sept">12 Sept</IonSelectOption>
+              <IonSelectOption value="13 Sept">13 Sept</IonSelectOption>
             </IonSelect>
           </IonItem>
         </IonList>
@@ -91,96 +129,48 @@ const CheckPlanPage: React.FC = () => {
             },
           }}
         >
-          <TimelineItem>
-            <TimelineOppositeContent color="textSecondary">
-              09:30
-            </TimelineOppositeContent>
-            <TimelineSeparator>
-              <TimelineDot variant="outlined" />
-              <TimelineConnector />
-            </TimelineSeparator>
-            <TimelineContent>
-              <div className="timeline_content">
-                <div className="titem_info">
-                  <span className="titem_activity">Eat</span>
-                  <span className="titem_location">
-                    <IonIcon icon={locationSharp} /> Florbela
-                  </span>
-                </div>
-                <a>
-                  <Icon icon="ic:baseline-directions" />
-                </a>
-              </div>
-            </TimelineContent>
-          </TimelineItem>
-          <TimelineItem>
-            <TimelineOppositeContent color="textSecondary">
-              10:00
-            </TimelineOppositeContent>
-            <TimelineSeparator>
-              <TimelineDot />
-              <TimelineConnector />
-            </TimelineSeparator>
-            <TimelineContent>
-              <div className="timeline_content">
-                <div className="titem_info">
-                  <span className="titem_activity">Visit Coloseum</span>
-                  <span className="titem_location">
-                    <IonIcon icon={locationSharp} /> Coloseum
-                  </span>
-                </div>
-                <a>
-                  <Icon icon="ic:baseline-directions" />
-                </a>
-              </div>
-            </TimelineContent>
-          </TimelineItem>
-          <TimelineItem>
-            <TimelineOppositeContent color="textSecondary">
-              11:00
-            </TimelineOppositeContent>
-            <TimelineSeparator>
-              <TimelineDot />
-              <TimelineConnector />
-            </TimelineSeparator>
-            <TimelineContent>
-              <div className="timeline_content">
-                <div className="titem_info">
-                  <span className="titem_activity">Code</span>
-                  <span className="titem_location">
-                    <IonIcon icon={locationSharp} /> Florbela
-                  </span>
-                </div>
-                <a>
-                  <Icon icon="ic:baseline-directions" />
-                </a>
-              </div>
-            </TimelineContent>
-          </TimelineItem>
-          <TimelineItem>
-            <TimelineOppositeContent color="textSecondary">
-              13:00
-            </TimelineOppositeContent>
-            <TimelineSeparator>
-              <TimelineDot />
-            </TimelineSeparator>
-            <TimelineContent>
-              <div className="timeline_content">
-                <div className="titem_info">
-                  <span className="titem_activity">Lunch</span>
-                  <span className="titem_location">
-                    <IonIcon icon={locationSharp} /> Pizzaria La Rua
-                  </span>
-                </div>
-                <a>
-                  <Icon icon="ic:baseline-directions" />
-                </a>
-              </div>
-            </TimelineContent>
-          </TimelineItem>
+          {filteredEvents.length === 0 ? (
+            <TimelineItem>
+              <TimelineContent>No events for this date</TimelineContent>
+            </TimelineItem>
+          ) : (
+            filteredEvents.map((event, index) => {
+              // Calculate the start time for each event
+              const eventDurationInMinutes = convertDurationToMinutes(event.duration);
+              const startTime = formatTime(accumulatedMinutes);
+              
+              // Update the accumulated time for the next event
+              accumulatedMinutes += eventDurationInMinutes;
+
+              return (
+                <TimelineItem key={index}>
+                  <TimelineOppositeContent color="textSecondary">
+                    {startTime}
+                  </TimelineOppositeContent>
+                  <TimelineSeparator>
+                    <TimelineDot variant="outlined" />
+                    {index !== filteredEvents.length - 1 && <TimelineConnector />}
+                  </TimelineSeparator>
+                  <TimelineContent>
+                    <div className="timeline_content">
+                      <div className="titem_info">
+                        <span className="titem_activity">{event.name}</span>
+                        <span className="titem_location">
+                          <IonIcon icon={locationSharp} /> {event.location}
+                        </span>
+                      </div>
+                      <a>
+                        <Icon icon="ic:baseline-directions" />
+                      </a>
+                    </div>
+                  </TimelineContent>
+                </TimelineItem>
+              );
+            })
+          )}
         </Timeline>
 
-        <Filters open={open} onClose={handleClose} />
+        <RegeneratePlan open={open} onClose={handleClose} />
 
         <IonFab slot="fixed" vertical="bottom" horizontal="end">
           <IonFabButton onClick={handleOpen}>
